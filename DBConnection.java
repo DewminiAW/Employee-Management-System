0c8248431f8ea239a0c1b81fcfb93b13558be294
Dewmini Wickramasooriya <abesinwickramasooriyadewmini@gmail.com>
2026-02-27 18:29:21 +0530

    database
    upload to heidi.sql

---
+++ DBConnection.java
@@ -0,0 +1,31 @@
+package org.Employe_System.util;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/employee_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (pstmt != null) pstmt.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
